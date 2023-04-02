649dc4154c2a8452cb4bf3bae5d393e4c1c0417d
Edpaul1 <saidarajab101@gmail.com>
2023-04-02 13:08:50 +0300

    Simple Liker App

--- main.js
+++ main.js
@@ -3,7 +3,31 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const onClick = document.getElementsByClassName('like');
+for (let i = 0; i < onClick.length; i++) {
+  onClick[i].addEventListener('click', function(){
+    if (onClick[i].classList.contains('activated-heart')){
+      onClick[i].classList.remove('activated-heart');
+      onClick[i].innerHTML = EMPTY_HEART;
+    } else {
+      mimicServerCall()
+      .then(() => {
+        onClick[i].classList.add('activated-heart');
+        onClick[i].innerHTML = FULL_HEART;
+      })
+      .catch(error => {
+        const errorModal = document.getElementById('modal');
+        const errorMessage = document.getElementById('modal-message');
+        errorMessage.textContent = error;
+        errorModal.classList.remove('hidden');
+        setTimeout(() => {
+          errorModal.classList.add('hidden');
+        }, 3000);
+      });
+    }
 
+  });
+}
 
 
 
